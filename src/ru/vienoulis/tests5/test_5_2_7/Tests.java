82240b14e0332935cf4e9db9bde03e8661988484
Лукьянов Роман Андреевич <vienoulis@gmail.com>
2020-02-24 22:17:26 +0300

    Java Core. Практическая задача 5.2.7 done

---
+++ src/ru/vienoulis/tests5/test_5_2_7/Tests.java
@@ -0,0 +1,24 @@
+package ru.vienoulis.tests5.test_5_2_7;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Tests {
+    public static void main(String[] args) throws IOException {
+        byte[] bytes = new byte[]{1, 2, 4, 10};
+        InputStream inputStream = new ByteArrayInputStream(bytes);
+        System.out.println(new Tests().sumOfStream(inputStream));
+
+
+    }
+
+    public int sumOfStream(InputStream inputStream) throws IOException {
+        int i;
+        int result = 0;
+        while ((i = inputStream.read()) != -1) {
+            result += (byte) i;
+        }
+        return result;
+    }
+}
